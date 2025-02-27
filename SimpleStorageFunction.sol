0b59564b33151dbc54247c6e511d08500aa5a188
leduong92 <maileduong92@gmail.com>
2025-02-27 15:38:25 +0700

    add data to struct

--- SimpleStorageFunction.sol
+++ SimpleStorageFunction.sol
@@ -2,15 +2,32 @@
 pragma solidity 0.8.26;
 
 contract SimpleStorageFunction {
-    uint256 public favoriteNumber; //public, private, internal, external
+    uint256 myFavoriteNumber; //public, private, internal, external
+
+    //uint256[] listOfFavoriteNumbers; 
     
+    struct Person {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    Person[] public listOfPeople; //[]
+
+    // Person public pat = Person({favoriteNumber: 7, name: "Pat"});
+    // Person public mariah = Person({favoriteNumber: 79, name: "Marial"});
+    // Person public jon = Person({favoriteNumber: 5, name: "Jon"});
+
     function store(uint256 _favoriteNumber) public {
-        favoriteNumber = _favoriteNumber;
+        myFavoriteNumber = _favoriteNumber;
     }
 
     //view: reading state, canot modify
     //pure: can't read state or modify
     function retrive() public view returns(uint256) {
-        return favoriteNumber;
+        return myFavoriteNumber;
+    }
+
+    function addPerson(string memory name, uint256 _favoriteNumber) public {
+        listOfPeople.push(Person(_favoriteNumber, name));
     }
 }
