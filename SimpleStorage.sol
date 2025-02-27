a4b84585ccf3a8cd8b66a0b3fe74c64dfa8e2f42
leduong92 <maileduong92@gmail.com>
2025-02-27 14:25:44 +0700

    data types

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -2,5 +2,19 @@
 pragma solidity ^0.8.26;
 
 contract SimpleStorage {
-    
+    // Define variables of different types
+    uint256 myUint = 123;
+    bool myBool = true;
+    string myString = "Hello";
+    address payable myAddress = payable(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4);
+    bytes32 myBytes = "hello";
+
+    // Define arrays of different types
+    uint256[] myUintArray = [1, 2, 3];
+    bool[] myBoolArray = [true, false, true];
+    string[] myStringArray = ["Apple", "Bannana", "Cherry"];
+    address payable[] myAddressArray = [
+        payable(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4),
+        payable(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4)
+    ];
 }
