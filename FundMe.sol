a719b2ec3a7eb2ef340447d7c03d2f62429596a5
leduong92 <maileduong92@gmail.com>
2025-03-03 11:41:50 +0700

    get version real MM wallet

--- FundMe.sol
+++ FundMe.sol
@@ -1,11 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
 contract FunMe {
-    uint256 public myValue = 1;
+    
+    uint256 public munimumUsd = 5;
 
     function fund() public payable {
-        myValue = myValue + 2;
-        require(msg.value > 1e18, "did'nt send enough ETH");
+        require(msg.value > munimumUsd, "did'nt send enough ETH");
     }
+
+    function getPrices() public {
+        //0x694AA1769357215DE4FAC081bf1f309aDC325306
+        //ABI
+        
+    }
+    function getConversionPrice(uint256 _price) public {
+
+    }
+
+    function getVersion() public view returns (uint256) {
+        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+    }
+
 }
