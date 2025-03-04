79852e0c850d3187421fcf29668a61b58aa1f8f4
leduong92 <maileduong92@gmail.com>
2025-03-04 13:39:24 +0700

    fund sepolia eth

--- FundMe.sol
+++ FundMe.sol
@@ -5,19 +5,30 @@ import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interf
 
 contract FunMe {
     
-    uint256 public munimumUsd = 5;
+    uint256 public munimumUsd = 5e18; //USD
 
     function fund() public payable {
-        require(msg.value > munimumUsd, "did'nt send enough ETH");
+        require(getConversionRate(msg.value) > munimumUsd, "did'nt send enough ETH");
     }
 
-    function getPrices() public {
+    function getPrices() public view returns (uint256) {
         //0x694AA1769357215DE4FAC081bf1f309aDC325306
         //ABI
-        
-    }
-    function getConversionPrice(uint256 _price) public {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (,int256 price,,,) = priceFeed.latestRoundData();
+        //Pirce of ETH in USD
+        //2000.00000000
+        return uint256(price * 1e10);
 
+    }
+    function getConversionRate(uint256 _ethAmount) public view returns (uint256) {
+        //1 ETH ?
+        //2000_000000000000000000
+        uint256 ethPirce = getPrices();
+        //(2000_000000000000000000 * 1_000000000000000000) / 1e18
+        //$2000 = 1ETH
+        uint256 ethAmountInUsd = (ethPirce * _ethAmount) / 1e18;
+        return ethAmountInUsd;
     }
 
     function getVersion() public view returns (uint256) {
